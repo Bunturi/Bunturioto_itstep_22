b84d7351f75eec57fc0e734a387332c3297d4770
bunturioto <Bunturioto@gmail.com>
2024-03-02 14:45:35 +0400

    Check if release year is less than 2000 and genre is Drama Update genre to 'Old_Drama' if it was 'Drama', otherwise keep the genre unchanged

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ for data in python_data:
             # Update genre to 'New_Crime' if it was 'Crime', otherwise keep the genre unchanged
             movie['genres'] = ['New_Crime' if genre == 'Crime' else genre for genre in movie['genres']]
             new_movies.append(movie)
+
+        # Check if release year is less than 2000 and genre is Drama
+        elif movie['release_date'][:4] < '2000' and 'Drama' in movie['genres']:
+            # Update genre to 'Old_Drama' if it was 'Drama', otherwise keep the genre unchanged
+            movie['genres'] = ['Old_Drama' if genre == 'Drama' else genre for genre in movie['genres']]
+            new_movies.append(movie)
