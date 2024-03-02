9a172e52423a1050372e7ade1f6afaa99f94dcd0
bunturioto <Bunturioto@gmail.com>
2024-03-02 14:42:19 +0400

    Iterate over each data entry in the loaded JSON Check if release year is greater than 2000 and genre is Crime Update genre to 'New_Crime' if it was 'Crime', otherwise keep the genre unchanged

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import json
 # Open the JSON file for reading
 with open("movies.json", "r", encoding="utf-8") as read_json:
     python_data = json.load(read_json)
+
+# Initialize a list to store updated movie data
+new_movies = []
+
+# Iterate over each data entry in the loaded JSON
+for data in python_data:
+    for movie in data['results']:
+        # Check if release year is greater than 2000 and genre is Crime
+        if movie['release_date'][:4] > '2000' and 'Crime' in movie['genres']:
+            # Update genre to 'New_Crime' if it was 'Crime', otherwise keep the genre unchanged
+            movie['genres'] = ['New_Crime' if genre == 'Crime' else genre for genre in movie['genres']]
+            new_movies.append(movie)
